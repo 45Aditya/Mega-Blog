002bb8544f98961b500afdc16c0adc1877f6f73d
Ujjwal <45ujjwal@gmail.com>
2024-11-01 20:45:13 +0530

    configure Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components/index'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Mega Blog with Appwrite</h1>
-    </>
-  )
+  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch() // Corrected 'disPatch' to 'dispatch'
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO{/* outlet */} 
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
